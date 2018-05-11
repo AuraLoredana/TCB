b34f063c163c2b25a2eb2f8e5e746f5d571247aa
AuraLoredana <popescuauraloredana@gmail.com>
2018-05-11 15:07:43 +0300

    contact page added and also the connection with Firebase

--- app/src/main/java/com/aura/popescu/tcb/Contact.java
+++ app/src/main/java/com/aura/popescu/tcb/Contact.java
@@ -1,7 +1,14 @@
 package com.aura.popescu.tcb;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Contact extends AppCompatActivity {
 
@@ -9,5 +16,20 @@ public class Contact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        ((Button) findViewById(R.id.btnOK)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String[] to = {"asociatiataxiulcubomboane@gmail.com"};
+                //String to = ((EditText)findViewById(R.id.txtTo)).getText().toString();
+                String sub = ((EditText)findViewById(R.id.txtSubject)).getText().toString();
+                String mess = ((EditText)findViewById(R.id.txtMessage)).getText().toString();
+                Intent mail = new Intent(Intent.ACTION_SEND);
+                mail.putExtra(Intent.EXTRA_EMAIL, to);
+                mail.putExtra(Intent.EXTRA_SUBJECT, sub);
+                mail.putExtra(Intent.EXTRA_TEXT, mess);
+                mail.setType("message/rfc822");
+                startActivity(Intent.createChooser(mail, "Send email via:"));
+            }
+        });
     }
 }
